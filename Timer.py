42673271c7f5e17cf42a81e53d142967ac10967f
Rafael Escoto <42154581+Hiraviel@users.noreply.github.com>
2019-05-21 07:53:17 -0500

    Add files via upload

--- Timer.py
+++ Timer.py
@@ -1,85 +1,155 @@
 import datetime
 import re
-from time import sleep
-import Tkinter_Timer
+from tkinter import Tk, Label, Button, Frame
 
-global stage1
+stage1 = 0
+proceso = 0
+flag_1 = False
 
 
 def cronometro():
-    Tkinter_Timer.root.mainloop()
+
+    global proceso
+
+    def iniciar(h=00, m=00, s=00):
+
+        global flag_1
+
+        if s >= 60:  # 60
+            s = 00
+            m = m + 1
+            if m >= 60:
+                m = 00
+                h = h + 1
+                if h >= 24:
+                    h = 00
+
+        if flag_1 == False:
+
+            if m < 5:  # 27
+                time["bg"] = "green"
+                estatus['text'] = "OK"
+                estatus['bg'] = "green"
+
+            elif m >= 5 and m < 10 :
+                time["bg"] = "yellow"
+                estatus['text'] = "OK"
+                estatus['bg'] = "yellow"
+
+            elif m>=10 and h >= 0:
+                flag_1 = True
+
+        if flag_1 == True:
+            time["bg"] = "red"
+            estatus['text'] = "NOK"
+            estatus['bg'] = "red"
+            flag_1 = True
+
+        time['text'] = str(h) + ":" + str(m) + ":" + str(s)
+
+        proceso = time.after(1000, iniciar, (h), (m), (s + 1))
+
+    def parar():
+        global proceso
+        time.after_cancel(proceso)
+
+    root = Tk()
+    root.title('Cronometro')
+    root.geometry("300x250+1000+100")
+    time = Label(root, fg='black', width=20, font=("Arial", "80"))
+    time.pack()
+    estatus = Label(root, fg='black', width=20, font=("Arial", "80"))
+    estatus.pack()
+    iniciar()
+    frame = Frame(root)
+    frame.pack()
+    root.mainloop()
 
 
 def captura():
 
-    stage1 = 0
+    global stage1
+    global flag_1
+
+    flag_1 = False
 
     while stage1 <= 0:
+
+        #print(stage1)
+        #print(flag_1)
+
         VIN = input("Put VIN: ")
         pkykVIN = bool(re.match("^[A-Z0-9]*$", VIN)) #OK
-        print(pkykVIN)
-        if len(VIN) != 17:
+        #print(pkykVIN)
+
+        if len(VIN) != 4: # CAMBIAR A 17
             print("Error with the lenght ({}) of the VIN.".format(len(VIN)))
+
         else:
+
             if pkykVIN != False:
-                print(stage1)   #OK
+                #print(stage1)   #OK
                 print("\n")
 
                 MODEL = input("Put MODEL: ")
                     
                 if len(MODEL) != 6:
                     print(len(MODEL))
-                    print(stage1)
+                    #print(stage1)
                     print("\n")
                     print("Error with the lenght ({}) of the MODEL.".format(len(MODEL)))
+
                 else:
                     stage1 = 1
+                    #break
 
-                if stage1 == 1:
-                    fecha = datetime.datetime.today().strftime("%Y-%d-%m")
+    #print(stage1)
 
-                    start = datetime.datetime.now()
-                    inicio = str(start)
+    if stage1 == 1:
 
-                    #sleep(2)
+        fecha = datetime.datetime.today().strftime("%Y-%d-%m")
 
-                    #Cronometro aquí
-                    cronometro()
+        start = datetime.datetime.now()
+        inicio = str(start)
+
+        #Cronometro aquí
+        cronometro()
+        stage1 = 0
+
+        end = datetime.datetime.now()
+        fin = str(end)
+
+        duration = end - start
+        tt = str(duration)
+        print(duration)
+
+    if tt != "00:54:00":
+        estatus = "OK"
+        print("OK")
+    else:
+        estatus = "NOK"
+        print("NOK")
 
 
-                    end = datetime.datetime.now()
-                    fin = str(end)
+    datos = (str(fecha),str(VIN), str(MODEL), str(inicio[11:19]), str(fin[11:19]), str(tt[0:7]), str(estatus))
+    print("Creating file..."+"\n")
+    file = open(VIN + "_" + fecha + ".txt", "w+")
 
-                    duration = end - start
-                    tt = str(duration)
-                    print(duration)
+    for i in range(len(datos)):
+        file.write(datos[i]+", ")
+    file.close()
 
-                    if tt != "00:54:00":
-                        estatus = "OK"
-                        print("OK")
-                    else:
-                        estatus = "NOK"
-                        print("NOK")
+    print("Done..."+"\n")
+
+    rfile = open(VIN + "_" + fecha + ".txt", "r")
+    if rfile.mode == "r":
+        contents = rfile.read()
+        print(contents)
 
 
-                    datos = (str(fecha),str(VIN), str(inicio[11:19]), str(fin[11:19]), str(tt[0:7]), str(estatus))
-                    print("Creating file..."+"\n")
-                    file = open(VIN + "_" + fecha + ".txt", "w+")
+    #print(stage1)
+    #print(flag_1)
 
-                    for i in range(len(datos)):
-                        file.write(datos[i]+", ")
-                    file.close()
-
-                    print("Done..."+"\n")
-
-                    rfile = open(VIN + "_" + fecha + ".txt", "r")
-                    if rfile.mode == "r":
-                        contents = rfile.read()
-                        print(contents)
-
-                    stage1 = 0
-
-                    print(stage1)
 
 if __name__ == "__main__":
     while True:
